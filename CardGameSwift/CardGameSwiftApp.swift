ec1f3e73160507df8d49e2680ebe2df0e00b39a9
HackX-IN <inamul2002@gmail.com>
2023-10-13 20:46:52 +0530

    Initial Commit

---
+++ CardGameSwift/CardGameSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameSwiftApp.swift
+//  CardGameSwift
+//
+//  Created by Inam 07 on 13/10/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct CardGameSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

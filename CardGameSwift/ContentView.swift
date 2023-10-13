1970332361881d9e035e98922f2fe3b0390492d0
HackX-IN <inamul2002@gmail.com>
2023-10-13 21:17:34 +0530

    Card game

--- CardGameSwift/ContentView.swift
+++ CardGameSwift/ContentView.swift
@@ -8,14 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var playerCard="card11"
+    @State var cpuCard="card13"
+    @State var PlayerScore:Int=0
+    @State var CpuScore:Int=0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [Color.purple, Color.blue]), startPoint: .bottomLeading, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/).ignoresSafeArea().aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+            VStack{
+                Image("logo").padding()
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image(playerCard).padding()
+                    
+                    Image(cpuCard)
+                    Spacer()
+                }
+                Spacer()
+                Button(action: {
+                    Play()
+                }, label: {
+                    Image("button")
+                })
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("Player").padding(.bottom,7)
+                        Text(String(PlayerScore))
+                    }.padding()
+                   
+                    VStack{
+                        Text("CPU").padding(.bottom,7)
+                        Text(String(CpuScore))
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
         }
-        .padding()
+    }
+    
+    func Play(){
+        let PlayerchangeCard=Int.random(in: 3...13)
+        playerCard="card"+String(PlayerchangeCard)
+        let CpuCard=Int.random(in: 3...13)
+        cpuCard="card"+String(CpuCard)
+        
+        if PlayerchangeCard > CpuCard {
+            PlayerScore+=1
+        }
+        else if CpuCard > PlayerchangeCard {
+            CpuScore+=1
+        }
+        
+      
     }
 }
 
